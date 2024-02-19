6717ac22c2262d42012db17f58d1d4a371badce1
sonng17 <ndson1703@gmail.com>
2024-02-20 03:14:00 +0700

    fix: update navigate

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,9 +2,11 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
+import {useNavigate} from 'react-router-dom'
 
 export default function OAuth() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -25,8 +27,9 @@ export default function OAuth() {
       });
       const data = await res.json();
       dispatch(signInSuccess(data));
+      navigate('/')
     } catch (error) {
-      console.log("");
+      console.log("could not sign in with google");
     }
   };
   return (
