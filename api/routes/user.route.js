40b08647a61b7c0bacf57c74929f3aa2fdafc0d2
sonng17 <ndson1703@gmail.com>
2024-02-29 10:29:32 +0700

    add delete user functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test1, test2, updateUser } from "../controllers/user.controller.js";
+import { test1, test2, updateUser, deleteUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 // create user router
@@ -8,5 +8,6 @@ const router = express.Router();
 router.get("/test-1", test1); // create route 1
 router.get("/test-2", test2); // create route 2
 router.post("/update/:id",verifyToken, updateUser); // create route 2
+router.delete('/delete/:id', verifyToken, deleteUser)
 
 export default router;
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -33,6 +33,18 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -43,5 +55,8 @@ export const {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserFailure,
+  deleteUserSuccess,
+  deleteUserStart,
 } = userSlice.actions;
 export default userSlice.reducer;
